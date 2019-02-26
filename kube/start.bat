21b69a5df8cb339d5ca430e4cac1f3b30aceece6
Ilija Vukotic <ivukotic@cern.ch>
2019-02-25 21:13:28 -0600

    fixes

--- kube/start.bat
+++ kube/start.bat
@@ -1,6 +1,6 @@
 kubectl create -f namespace.yaml
 
 kubectl create secret -n gates generic cert-secret --from-file=key=secrets/certificates/gates.key.pem --from-file=cert=secrets/certificates/gates.cert.cer
-kubectl create secret -n gates generic config --from-file=conf=secrets/config.json
+kubectl create secret -n gates generic config --from-file=conf=config.json
 
 kubectl create -f frontend.yaml
